d98cdf846eb7241f7415e8c5f4a57f03ed11cc93
alreimar <alreimar@gmail.com>
2017-03-17 09:42:10 +0200

    Create JavaRush0437.java
    Используя цикл for вывести на экран прямоугольный треугольник из восьмёрок со сторонами 10 и 10.

---
+++ JavaRush0437.java
@@ -0,0 +1,35 @@
+/* 
+Треугольник из восьмерок
+*/
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+     // int i = 0;
+      //int j = 0;
+        /*System.out.println(i+ ""+ j);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String first = reader.readLine();
+        int t = Integer.parseInt(first);
+
+        int p = Integer.parseInt(reader.readLine());
+
+        System.out.println(p + "" + t);*/
+
+
+        for (int i = 1; i<=10;i++ ) {
+
+
+
+               for (int j= 0; j <i; j=j+1 ) {
+                   System.out.print("8");
+
+
+               }System.out.println( "");
+                                                }
+
+
+
+
+    }
+}
